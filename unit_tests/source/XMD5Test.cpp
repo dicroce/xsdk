3683c9a244d3b80cfc20c6490f3a6d96c0946423
Tony Di Croce <dicroce@gmail.com>
2014-11-25 14:10:45 -0800

    initial checkin of new unit test framework.

---
+++ unit_tests/source/XMD5Test.cpp
@@ -0,0 +1,19 @@
+
+#include "XMD5Test.h"
+#include "XSDK/XException.h"
+
+using namespace XSDK;
+
+REGISTER_TEST_FIXTURE(XMD5Test);
+
+void XMD5Test::setup()
+{
+}
+
+void XMD5Test::teardown()
+{
+}
+
+void XMD5Test::TestConstructor()
+{
+}
